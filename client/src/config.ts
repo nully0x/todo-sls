63af92ee95151b9ff2d35a0a72c2f7e6fb010d57
crytochap <olusegunsamson@live.com>
2022-07-24 09:44:54 +0100

    Complete auth, serveless and deployment

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'twgojoflq8'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-wdza0-ct.us.auth0.com',
+  clientId: 'NdMF03CyAY8PUTQMHsbwbZL74TsxRe73',
+  callbackUrl: 'http://localhost:3000/callback'
+}
